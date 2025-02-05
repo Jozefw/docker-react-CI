bef4a6e1b7300bbc2429a573caf5d18b0bf70e7f
Jozef <Jozefw@sbcglobal.net>
2025-02-05 14:08:22 -0800

    first commit

---
+++ src copy/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Woo Hoo or Yaaa hoo
+      </header>
+    </div>
+  );
+}
+
+export default App;
